714376616f3b3c26d933e2807bd81a074728acb5
Markus Tran <thebluepillow@gmail.com>
2017-11-06 20:28:24 -0800

    remove define

--- index.js
+++ index.js
@@ -5,3 +5,8 @@ module.exports = sowpods
 sowpods.anagram = require('./src/anagram')
 // sowpods.define = require('./src/define')
 sowpods.suggest = require('./src/suggest')
+
+// Temporary placeholder
+sowpods.define = function define (word, callback) {
+  return setImmediate(callback, new Error('`define` not implemented'))
+}
--- src/define.js
+++ src/define.js
@@ -1,37 +1,41 @@
-const _ = require('lodash');
-const request = require('superagent');
-const cheerio = require('cheerio');
+const request = require('superagent')
+const cheerio = require('cheerio')
 
-module.exports = function define(search, callback) {
-  if (!/^[a-zA-Z]+$/.test(search))
-    return setImmediate(callback, `\`${search}\` must be an alphabetic string`);
+module.exports = function define (word, callback) {
+  if (!/^[a-z]+$/i.test(word)) {
+    return setImmediate(callback, new Error(`\`${word}\` must be an alphabetic string`))
+  }
 
   request
     .post('http://scrabble.hasbro.com/en-us/tools#dictionary')
-    .type('application/x-www-form-urlencoded')
-    .send('dictWord=' + search.toLowerCase())
-    .end(function(err, res) {
-      if (err)
-        return setImmediate(callback, err);
+    .type('form')
+    .send({ dictWord: word.toLowerCase() })
+    .end(function (error, response) {
+      if (error) {
+        return setImmediate(callback, error)
+      }
 
-      const html = cheerio.load(res.text)('.word-definition').html();
+      const $ = cheerio.load(response.text)
+      const element = $('.word-definition').html()
 
       // If not a valid Scrabble word, OOPS! is returned
-      if (!html || /oops!/i.test(html))
-        return setImmediate(callback, `\`${search.toLowerCase()}\` not found`);
+      if (!element || /oops!/i.test(element)) {
+        console.log(response.text)
+        return setImmediate(callback, `\`${word.toLowerCase()}\` not found`)
+      }
 
-      const condensed = html.replace(/(\t|\n)+/g, '');
+      const condensed = element.replace(/[\t\n]+/g, '')
 
       // Example html:
       // "<h4>MOO</h4>to make the deep, moaning sound of a cow<p>Related Words: <strong>MOOED/MOOING/MOOS</strong></p>"
-      const word = condensed.match(/<h4>(.*?)<\/h4>/);
-      const definition = condensed.match(/<\/h4>(.*?)<p>/);
-      const related = condensed.match(/<strong>(.*?)<\/strong>/);
+      const _word = condensed.match(/<h4>(.*?)<\/h4>/)
+      const definition = condensed.match(/<\/h4>(.*?)<p>/)
+      const related = condensed.match(/<strong>(.*?)<\/strong>/)
 
       setImmediate(callback, null, {
-        word: word && word[1].trim(),
+        word: _word && _word[1].trim(),
         definition: definition && definition[1].trim(),
-        related: related && _.filter(related[1].split('/').map(_.partial(_.trim, _)))
-      });
-    });
-};
+        related: related && related[1].split('/').map(x => x.trim()).filter(x => x)
+      })
+    })
+}
